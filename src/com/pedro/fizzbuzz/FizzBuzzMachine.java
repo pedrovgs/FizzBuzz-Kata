1dd9a5c7687f4b1edeb00ccff4881b6d3560db37
pedrovgs <pedrovicente.gomez@gmail.com>
2014-02-19 01:14:50 +0100

    Add the default case to the documentation

--- src/com/pedro/fizzbuzz/FizzBuzzMachine.java
+++ src/com/pedro/fizzbuzz/FizzBuzzMachine.java
@@ -8,10 +8,24 @@ package com.pedro.fizzbuzz;
  * If the integer passed as argument it's equals to 3 or multiple of 3 FizzBuzzMachine returns Fizz.
  * If the integer passed as argument it's equals to 5 or multiple of 5 FizzBuzzMachine returns Buzz.
  * If the integer passed as argument it's multiple of 3 and 5 FizzBuzzMachine returns FizzBuzz.
+ * If the integer passed as argument isn't multiple of 3 or 5 returns the number.
  *
  * @author Pedro Vicente Gómez Sánchez.
  */
 public class FizzBuzzMachine {
 
-
+    /**
+     * Main FizzBuzzMachine method. This method will check if the number passed as parameter is fulfilling the condition
+     * and will return the Fizz, Buzz or FizzBuzz value.
+     *
+     * @param value to analyze.
+     * @return
+     *      Fizz if the value is multiple of 3
+     *      Buzz if the value is multiple of 5
+     *      FizzBuzz if the value is multiple of 3 and 5
+     *      the value if the parameter isn't multiple of 3 or 5
+     */
+    public String execute(int value) {
+        return null;
+    }
 }
