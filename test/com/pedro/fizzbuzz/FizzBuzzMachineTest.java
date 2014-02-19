9847838c4e3244a688db9e81049778839f887249
pedrovgs <pedrovicente.gomez@gmail.com>
2014-02-19 01:14:33 +0100

    Start writing some tests obtained from the documentation

--- test/com/pedro/fizzbuzz/FizzBuzzMachineTest.java
+++ test/com/pedro/fizzbuzz/FizzBuzzMachineTest.java
@@ -3,6 +3,8 @@ package com.pedro.fizzbuzz;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Test class created to check the correct behaviour of FizzBuzzMachine program. FuzzBuzzMachine it's a little program
  * created to return some console output when some of the next conditions appears in a set of integers. The behaviour
@@ -13,8 +15,17 @@ import org.junit.Test;
 public class FizzBuzzMachineTest {
 
     /*
+     * Constants
+     */
+
+    private static final String FIZZ = "FIZZ";
+    private static final String BUZZ = "BUZZ";
+    private static final String FIZZBUZZ = "FIZZBUZZ";
+
+    /*
      * Test data
      */
+
     private FizzBuzzMachine fizzBuzzMachine;
 
     /*
@@ -31,13 +42,39 @@ public class FizzBuzzMachineTest {
      */
 
     @Test
-    public void test() {
-
+    public void shouldReturnFizzWithTheValueThree() {
+        assertEquals(FIZZ, fizzBuzzMachine.execute(3));
     }
 
-    /*
-     * Auxiliary methods
-     */
+    @Test
+    public void shouldReturnBuzzWithTheValueFive() {
+        assertEquals(BUZZ, fizzBuzzMachine.execute(5));
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzWithTheValueFifteen() {
+        assertEquals(FIZZBUZZ, fizzBuzzMachine.execute(15));
+    }
+
+    @Test
+    public void shouldReturnTheValueWithNoMultipleValue() {
+        assertEquals("7", fizzBuzzMachine.execute(7));
+    }
+
+    @Test
+    public void shouldReturnFizzWithAMultipleOfThree() {
+        assertEquals(FIZZ,fizzBuzzMachine.execute(9));
+    }
+
+    @Test
+    public void shouldReturnBuzzWithAMultipleOfFive() {
+        assertEquals(BUZZ,fizzBuzzMachine.execute(50));
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzWithAMultipleOfThreeAndFive() {
+        assertEquals(FIZZBUZZ,fizzBuzzMachine.execute(90));
+    }
 
 
 }
